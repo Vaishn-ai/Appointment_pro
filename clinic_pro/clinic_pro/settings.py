d70a1a63055c51cc7412a3a30c1c01b1d5abd48e
Vaishnavi dandekar <vaishnavidandekar81@gmail.com>
2026-02-07 13:23:56 +0530

    fixed static settings

--- clinic_pro/clinic_pro/settings.py
+++ clinic_pro/clinic_pro/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z07cgc=p)7f6snc@um6$=foa75v+r@q!vwrt__ap4ttz_9evgk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Vaishn-ai.pythonanywhere.com']
 
 
 # Application definition
@@ -123,10 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
